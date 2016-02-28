67746f15e00908f018be6a4ef841f38a05be8e04
Dušan Plavák <dusan.plavak@gmail.com>
2016-02-28 23:19:37 +0100

    Initial commit este framework.

---
+++ src/browser/app/Header.react.js
@@ -0,0 +1,32 @@
+import Component from 'react-pure-render/component';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+
+  static propTypes = {
+    msg: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { msg } = this.props;
+
+    return (
+      <header>
+        <h1>
+          <Link to="/">{msg.home}</Link>
+        </h1>
+        <ul>
+          <li><Link activeClassName="active" to="/firebase">{msg.firebase}</Link></li>
+          <li><Link activeClassName="active" to="/me">{msg.me}</Link></li>
+        </ul>
+      </header>
+    );
+  }
+
+}
+
+export default connect(state => ({
+  msg: state.intl.msg.app.links,
+}))(Header);
